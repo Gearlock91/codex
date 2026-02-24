4119ff274409d7ce27ffcc315ff2ab46e97e7c88
Andreas Roghe <andreas.roghe@gmail.com>
2026-02-24 16:03:41 +0100

    Add test for nullopt

--- tests/env_test.cpp
+++ tests/env_test.cpp
@@ -7,3 +7,8 @@ TEST(EnvStr, GetEnvString) {
     codex::env::load("test");
     EXPECT_EQ(codex::env::get<std::string>("TEST"), std::optional<std::string>("123"));
 }
+
+TEST(EnvStr, GetUnknown) {
+    EXPECT_EQ(codex::env::get<std::string>("UNKNOWN"), std::nullopt);
+    EXPECT_EQ(codex::env::get<std::string>("UNKNOWN").value_or("13"), "13");
+}
